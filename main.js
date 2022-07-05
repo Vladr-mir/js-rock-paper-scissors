bb2f454ffda07817ff41b5088433e564714b1833
Vladimir <duenasvladimir@gmail.com>
2022-07-05 17:11:33 -0600

    main.js roundPlay() function finished

--- main.js
+++ main.js
@@ -5,38 +5,83 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection){
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  round_value = {"playerScore": 0, "computerScore": 0, "message": ``};
+
   if (playerSelection == computerSelection) {
-    return 0
+    round_value["message"] = `invalid value`;
+    return round_value;
   }
 
   switch (playerSelection) {
     case "rock":
       if (computerSelection == "paper") {
-        return -1;
+        round_value["computerScore"] = 1;
+        round_value["message"] = `${computerSelection} defeats ${playerSelection}`;
       }
       if (computerSelection == "scissors") {
-        return 1;
+        round_value["playerScore"] = 1;
+        round_value["message"] = `${playerSelection} defeats ${computerSelection}`;
       }
       break;
     
     case "paper":
       if (computerSelection == "rock") {
-        return 1;
+        round_value["playerScore"] = 1;
+        round_value["message"] = `${playerSelection} defeats ${computerSelection}`;
       }
       if (computerSelection == "scissors") {
-        return -1;
+        round_value["computerScore"] = 1;
+        round_value["message"] = `${computerSelection} defeats ${playerSelection}`;
       }
       break;
 
     case "scissors":
       if (computerSelection == "rock") {
-        return -1;
+        round_value["computerScore"] = 1;
+        round_value["message"] = `${computerSelection} defeats ${playerSelection}`;
       }
       if (computerSelection == "paper") {
-        return 1;
+        round_value["playerScore"] = 1;
+        round_value["message"] = `${playerSelection} defeats ${computerSelection}`;
       }
-      
       break;
+    default:
+      round_value["message"] = `invalid value`;
+  }
+
+  return round_value;
+}
+
+function game(rounds=5) {
+  playerScore = 0;
+  computerScore = 0;
+
+  playerSelection = "";
+  computerSelection = "";
+
+  round_value = {};
+
+  for (i = 1; i <= rounds; i++) {
+    playerSelection = prompt("Enter your selection: ");
+    computerSelection = computerPlay();
+    round_value = playRound(playerSelection, computerSelection);
+
+    playerScore += round_value["playerScore"];
+    computerScore += round_value["computerScore"];
+
+    console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+    console.log(round_value["message"]);
+  }
+
+  if (playerScore == computerScore) {
+    console.log("Tie!");
+  } else if (playerScore > computerScore) {
+    console.log("You Win!");
+  } else {
+    console.log("You lose!");
   }
 }
 
+game(5);
